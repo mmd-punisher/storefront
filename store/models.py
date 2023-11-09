06bffea34ff4191a71fa6780a1d98cb6fa99fafd
Mohammad Mousapour <mohammadmoosapoor4@gmail.com>
2023-11-09 14:47:21 +0330

    Revisiting Data Model

--- store/models.py
+++ store/models.py
@@ -1,5 +1,6 @@
 from django.core.validators import MinValueValidator
 from django.db import models
+from uuid import uuid4
 
 
 class Promotion(models.Model):
@@ -93,13 +94,16 @@ class Address(models.Model):
 
 
 class Cart(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4)
     created_at = models.DateTimeField(auto_now_add=True)
 
 
 class CarItem(models.Model):
-    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='item')
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveSmallIntegerField()
+    class Meta:
+        unique_together = [['cart', 'product']]
 
 
 class Review(models.Model):
