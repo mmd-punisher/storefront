07e4dd38c521ea239afbb16eeeb276425faa3508
Mohammad Mousapour <mohammadmoosapoor4@gmail.com>
2024-08-19 02:31:39 +0330

    feat: Adding a cart - Update a cart items

--- store/serializers.py
+++ store/serializers.py
@@ -62,3 +62,38 @@ class CartSerializer(serializers.ModelSerializer):
     class Meta:
         model = Cart
         fields = ['id', 'items', 'total_price']
+
+
+class AddCartItemSerializer(serializers.ModelSerializer):
+    product_id = serializers.IntegerField()
+
+    def validate_product_id(self, attrs):
+        if not Product.objects.filter(pk=attrs).exists():
+            raise serializers.ValidationError('Product does not exist')
+        return attrs
+
+    def save(self, **kwargs):
+        cart_id = self.context['cart_id']
+        product_id = self.validated_data['product_id']
+        quantity = self.validated_data['quantity']
+        try:
+            # update an existing item
+            cart_item = CartItem.objects.get(cart_id=cart_id, product_id=product_id)
+            cart_item.quantity += quantity
+            cart_item.save()
+            self.instance = cart_item
+        except CartItem.DoesNotExist:
+            # create a new item
+            self.instance = CartItem.objects.create(cart_id=cart_id, **self.validated_data)
+
+        return self.instance
+
+    class Meta:
+        model = CartItem
+        fields = ['id', 'product_id', 'quantity']
+
+
+class UpdateCartItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CartItem
+        fields = ['quantity']
--- store/views.py
+++ store/views.py
@@ -8,7 +8,8 @@ from rest_framework.viewsets import ModelViewSet, GenericViewSet
 from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin, ListModelMixin, DestroyModelMixin
 from .filters import ProductFilter
 from .models import Collection, Product, Review, Cart, CartItem
-from .serializers import CollectionSerializer, ProductSerializer, ReviewSerializer, CartSerializer, CartItemSerializer
+from .serializers import CollectionSerializer, ProductSerializer, ReviewSerializer, CartSerializer, CartItemSerializer, \
+    AddCartItemSerializer, UpdateCartItemSerializer
 from .pagination import DefaultPagination
 
 
@@ -63,7 +64,18 @@ class CartViewSet(CreateModelMixin, ListModelMixin, RetrieveModelMixin, DestroyM
 
 
 class CartItemViewSet(ModelViewSet):
-    serializer_class = CartItemSerializer
+    # serializer_class = CartItemSerializer
+    http_method_names = ['get', 'post', 'patch', 'delete']
 
     def get_queryset(self):
         return CartItem.objects.filter(cart_id=self.kwargs['cart_pk']).select_related('product')
+
+    def get_serializer_class(self):
+        if self.request.method == 'POST':
+            return AddCartItemSerializer
+        elif self.request.method == 'PATCH':
+            return UpdateCartItemSerializer
+        return CartItemSerializer
+
+    def get_serializer_context(self):
+        return {'cart_id': self.kwargs['cart_pk']}
