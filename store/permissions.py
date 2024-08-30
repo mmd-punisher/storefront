131641f6e40a4d6409df0c6a4d261fccd6921950
Mohammad Mousapour <mohammadmoosapoor4@gmail.com>
2024-08-31 02:00:15 +0330

    update: applying custom model permissions

--- store/permissions.py
+++ store/permissions.py
@@ -6,3 +6,15 @@ class IsAdminOrReadOnly(permissions.BasePermission):
         if request.method in permissions.SAFE_METHODS:
             return True
         return bool(request.user and request.user.is_staff)
+
+
+class FullDjangoModelPermissions(permissions.DjangoModelPermissions):
+    def __init__(self):
+        self.perms_map['GET'] = ['%(app_label)s.view_%(model_name)s']
+
+
+class ViewHistoryPermissions(permissions.DjangoModelPermissions):
+    def has_permission(self, request, view):
+        # Syntax: app_name.permission_name
+        # If this returns true, then the user is gonna had permission and will be able to  access the history
+        return request.user.has_perm('store.view_history')
--- store/views.py
+++ store/views.py
@@ -5,7 +5,7 @@ from rest_framework import status
 from rest_framework.decorators import action
 from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated, AllowAny, IsAdminUser
+from rest_framework.permissions import IsAuthenticated, AllowAny, IsAdminUser, DjangoModelPermissions
 from rest_framework.viewsets import ModelViewSet, GenericViewSet
 from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin, ListModelMixin, DestroyModelMixin, \
     UpdateModelMixin
@@ -14,7 +14,7 @@ from .models import Collection, Product, Review, Cart, CartItem, Customer
 from .serializers import CollectionSerializer, ProductSerializer, ReviewSerializer, CartSerializer, CartItemSerializer, \
     AddCartItemSerializer, UpdateCartItemSerializer, CustomerSerializer
 from .pagination import DefaultPagination
-from .permissions import IsAdminOrReadOnly
+from .permissions import IsAdminOrReadOnly, FullDjangoModelPermissions, ViewHistoryPermissions
 
 
 class ProductViewSet(ModelViewSet):
@@ -92,10 +92,16 @@ class CustomerViewSet(ModelViewSet):
     serializer_class = CustomerSerializer
     permission_classes = [IsAdminUser]
 
+    # permission_classes = [FullDjangoModelPermissions]
+
     # def get_permissions(self):
     #     if self.request.method == 'GET':
     #         return [AllowAny()]
     #     return [IsAuthenticated()]
+
+    @action(detail=True, permission_classes=[ViewHistoryPermissions])
+    def history(self, request, pk):
+        return Response('ok')
 
     @action(detail=False, methods=['GET', 'PUT'], permission_classes=[IsAuthenticated])
     def me(self, request):
